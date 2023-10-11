98490b501f929190c88ff30064c8631ce5b8cb54
Gabriel Monutti <gabimonutti1@gmail.com>
2023-10-11 01:17:59 -0300

    Create spring boot project

---
+++ tpi/src/main/java/g147bda2023/tpi/TpiBdaG1472023Application.java
@@ -0,0 +1,13 @@
+package g147bda2023.tpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiBdaG1472023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpiBdaG1472023Application.class, args);
+	}
+
+}
---
+++ tpi/src/test/java/g147bda2023/tpi/TpiBdaG1472023ApplicationTests.java
@@ -0,0 +1,13 @@
+package g147bda2023.tpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiBdaG1472023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
