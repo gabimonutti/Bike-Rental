cb94f23fe83d6bad007177f59b614deb38ea5b23
EzicStar <50255924+EzicStar@users.noreply.github.com>
2023-11-12 17:02:36 -0300

    Still working on rest template

---
+++ tpi/Alquileres/src/main/java/g38/tpi/bda2023/Alquileres/application/response/EstacionResponse.java
@@ -0,0 +1,4 @@
+package g38.tpi.bda2023.Alquileres.application.response;
+
+public class EstacionResponse {
+}
--- tpi/Alquileres/src/main/java/g38/tpi/bda2023/Alquileres/services/EstacionServiceImpl.java
+++ tpi/Alquileres/src/main/java/g38/tpi/bda2023/Alquileres/services/EstacionServiceImpl.java
@@ -1,7 +1,9 @@
 package g38.tpi.bda2023.Alquileres.services;
 
+import g38.tpi.bda2023.Alquileres.application.response.EstacionResponse;
 import g38.tpi.bda2023.Alquileres.models.Estacion;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
@@ -11,7 +13,15 @@ public class EstacionServiceImpl implements EstacionService {
             try {
                 RestTemplate template = new RestTemplate();
 
-                //ResponseEntity<>
+                ResponseEntity<EstacionResponse> res =
+                        template.getForEntity("http://localhost:8082/api/estaciones/{id}",
+                                EstacionResponse.class, id);
+                if(res.getStatusCode().is2xxSuccessful()) {
+                    return 
+                }
+            }
+            catch (HttpClientErrorException ex) {
+                return null;
             }
         }
 }
