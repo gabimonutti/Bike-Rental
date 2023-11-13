ed77eadf2cc915400a0eaa655ae0dea677e7e6b7
Gabriel Monutti <gabimonutti1@gmail.com>
2023-11-13 18:15:00 -0300

    Add error when exchange is not allowed

--- tpi/Alquileres/src/main/java/g38/tpi/bda2023/Alquileres/services/AlquilerApplicationServiceImpl.java
+++ tpi/Alquileres/src/main/java/g38/tpi/bda2023/Alquileres/services/AlquilerApplicationServiceImpl.java
@@ -24,6 +24,8 @@ public class AlquilerApplicationServiceImpl implements AlquilerApplicationServic
         Estacion estDevolucion = estacionService.findById(idEstacionDevolucion)
                 .orElseThrow(() -> new IllegalArgumentException("Estacion Devolucion Not Found"));
 
+        if (!(exchangeService.ExchangesAvailable.contains(moneda))) throw new IllegalArgumentException("Exchange not allowed");
+
         Alquiler alquiler = alquilerService.end(idAlquiler, estDevolucion);
         AlquilerResponse alquilerResponse = AlquilerResponse.from(alquiler);
 
