8f425d565dd05e1300006b72d1d87508d094f7e9
EzicStar <50255924+EzicStar@users.noreply.github.com>
2023-11-13 01:31:15 -0300

    Removed TODO from Start Rental
    This fare should be done in End Rental

--- tpi/Alquileres/src/main/java/g38/tpi/bda2023/Alquileres/services/AlquilerServiceImpl.java
+++ tpi/Alquileres/src/main/java/g38/tpi/bda2023/Alquileres/services/AlquilerServiceImpl.java
@@ -17,8 +17,6 @@ public class AlquilerServiceImpl implements AlquilerService{
     public Alquiler start(long idCliente, Estacion estRetiro) {
         long id = alquilerRepository.getMaxId() + 1;
         LocalDateTime fechaHoraRetiro = LocalDateTime.now();
-        // TODO: verificar si la fecha de retiro es promocional, si es asi asignarle la tarifa correspondiente, sino
-        // dejarlo en null y que se lo asigne en devolucion (ver enunciado)
         Tarifa tarifa = null;
         Alquiler alquiler = new Alquiler(id, idCliente, 1, estRetiro, null, fechaHoraRetiro, null, null,  tarifa);
         return alquilerRepository.save(alquiler);
